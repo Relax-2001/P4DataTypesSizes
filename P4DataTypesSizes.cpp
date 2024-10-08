1cb2ce9243700fbb67e5b47f5f6080af90259cc8
Mahmoud <Mahmoud@DESKTOP-H3U04SS>
2024-10-09 02:03:16 +0300

    Add project files.

---
+++ P4DataTypesSizes.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "The size of  bool data type is : " << sizeof(bool) << "\n";
+	cout << "The size of  char data type is : " << sizeof(char) << "\n";
+	cout << "The size of short data type is : " << sizeof(short) << "\n";
+	cout << "The size of int data type is : " << sizeof(int) << "\n";
+
+	cout << "The size of long int data type is : " << sizeof(long) << "\n";
+	cout << "The size of long long data type is : " << sizeof(long long) << "\n";
+
+	cout << "The size of float data type is : " << sizeof(float) << "\n";
+	cout << "The size of double data type is : " << sizeof(double) << "\n";
+
+	cout << "The size of long double data type is : " << sizeof(long double) << "\n";
+
+
+	return 0;
+
+}
